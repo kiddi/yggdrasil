75a28f26f819adcbffc5de520c27eca14a6bdc90
Kiddi <kiddi@plainvanillagames.com>
2016-03-26 14:59:58 +0000

    added basic function

---
+++ src/client/app/samthykkt.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const SAMTHYKKT = {
+  title: {
+    title: 'SAMÞYKKTIR'
+  },
+  articles: [{
+    number: 1,
+    text: 'Félagið er einkahlutafélag og er nafn þess '
+  },
+  {
+    number: 2,
+    text: 'Tilgangur félagsins er '
+  },
+  {
+    number: 3,
+    text: 'Hlutafé félagsins er kr. 500.000 - krónur fimm hundruð þúsund. Hver hlutur er að fjárhæð kr. 1 - króna  að nafnverði.'
+  },
+  {
+    number: 4,
+    text: 'Heimilt er að hækka hlutafé félagsins með ályktun hluthafafundar og þarf til sama magn atkvæða og til breytinga á samþykktum þessum. Hluthafar skulu hafa forgangsrétt að nýjum hlutum í hlutfalli við skráða hlutafjáreign sína. Hlutahafafundur einn getur ákveðið lækkun hlutafjár. Sé hluthafi einn getur hann að lagaskilyrðum fullnægðum skráð hækkun eða lækkun hlutafjár í gerðabók.'
+  }],
+}
+
+module.exports = SAMTHYKKT;
