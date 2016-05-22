f93eb7dc3c38d0fbdfacdace37451e2031830207
Kiddi <kiddi@plainvanillagames.com>
2016-05-22 14:23:45 +0000

    new look

--- src/client/app/samthykkt.js
+++ src/client/app/samthykkt.js
@@ -2,24 +2,105 @@
 
 const SAMTHYKKT = {
   title: {
-    title: 'SAMÞYKKTIR'
+    title: 'Samþykktir'
   },
-  articles: [{
-    number: 1,
-    text: 'Félagið er einkahlutafélag og er nafn þess '
+  articles: [
+    {
+      number: 1,
+      text: 'Félagið er einkahlutafélag og er nafn þess '
+    },
+    {
+      number: 2,
+      text: 'Tilgangur félagsins er '
+    },
+    {
+      number: 3,
+      text: 'Hlutafé félagsins er kr. 500.000 - krónur fimm hundruð þúsund. Hver hlutur er að fjárhæð kr. 1 - króna  að nafnverði.'
+    },
+    {
+      number: 4,
+      text: 'Heimilt er að hækka hlutafé félagsins með ályktun hluthafafundar og þarf til sama magn atkvæða og til breytinga á samþykktum þessum. Hluthafar skulu hafa forgangsrétt að nýjum hlutum í hlutfalli við skráða hlutafjáreign sína. Hlutahafafundur einn getur ákveðið lækkun hlutafjár. Sé hluthafi einn getur hann að lagaskilyrðum fullnægðum skráð hækkun eða lækkun hlutafjár í gerðabók.'
+    },
+    {
+      number: 5,
+      text: 'Stjórn félagsins skal halda hlutaskrá samkvæmt lögum.'
+    },
+    {
+      number: 6,
+      text: 'Eigendaskipti að hlutum í félaginu öðlast ekki gildi gagnvart því fyrr en stjórn þess hefur verið tilkynnt um það skriflega eða þau hafa verið færð í gerðabók einkahlutafélags eins aðila. Stjórn félagsins hefur forkaupsrétt fyrir félagsins hönd að fölum hlutum. Að félaginu frágengnu hefur hluthafi forkaupsrétt að hlutum í hlutfalli við hlutafjáreign sína. Verði ágreiningur um verð hluta skal það ákveðið með mati tveggja óvilhallra manna sem dómkvaddir skulu til þess starfa. Forkaupsréttarhafi hefur tveggja mánaða frest til að beita forkaupsrétti sínum og telst fresturinn frá tilkynningu til stjórnar um tilboð. Þá mega eigi líða fleiri en þrír mánuðir frá því að kaup voru ákveðin þar til kaupverð er greitt. Eigendaskipti vegna erfða eða búskipta lúta ekki framangreindum forkaupsréttarreglum. Óheimilt er að veðsetja eða gefa hluti í félaginu án samþykkis félagsstjórnar. Ef einkahlutafélag, sem er skráð í eigu fleiri en eins aðila, kemst á hendur eins aðila skal tilkynna hlutafélagaskrá það.'
+    },
+    {
+      number: 7,
+      text: 'Félagið má eigi veita  lán út á hluti sína nema lög leyfi.  Félaginu er heimilt að kaupa eigin hluti að því marki sem lög leyfa.  Óheimilt er að neyta atkvæðisréttar fyrir þá hluti sem félagið á sjálft.'
+    },
+    {
+      number: 8,
+      text: 'Hluthafi ber ekki ábyrgð á skuldbindingum félagsins fram yfir hlut sinn í félaginu.'
+    },
+    {
+      number: 9,
+      text: 'Æðsta vald í málefnum félagsins er í höndum lögmætra hluthafafunda séu hluthafar fleiri en einn en ella í höndum hluthafans, sbr. lög um einkahlutafélög (ehfl.).'
+    },
+    {
+      number: 10,
+      text: 'Aðalfundur skal haldinn fyrir lok ágúst ár hvert. Sé hluthafi einn þarf ekki að halda aðalfund heldur skal hluthafinn taka viðeigandi ákvarðanir skv. 59. gr. ehfl. og skrá þær í gerðabók skv. 55. gr. ehfl. Aukafundi skal halda eftir ákvörðun stjórnar eða að kröfu hluthafa sem ráða a.m.k. einum tíunda hluta hlutafjár í félaginu.  Skal krafan skriflega gerð og fundarefni tilgreint og fundur boðaður þá innan fjórtán daga.  Ef stjórnin skirrist við að boða fund eftir móttöku slíkrar kröfu má leita atbeina stjórnvalda samkvæmt lögum um einkahlutafélög. Sé hluthafi einn tekur hann afstöðu til mála, sem almennt er fjallað um á aukafundum, og skráir ákvarðanir í gerðabók.'
+    },
+    {
+      number: 11,
+      text: 'Félagsstjórn skal boða til hluthafafunda með tilkynningu til hvers hluthafa í ábyrgðarbréfi, tölvupósti gegn staðfestingu á móttöku eða á annan jafnsannanlegan hátt.  Aðalfund skal boða með minnst fjórtán daga fyrirvara en aukafund með minnst sjö daga fyrirvara.  Fundarefnis skal getið í fundarboði. Hluthafafundur er lögmætur ef hann er löglega boðaður og hann sækja hluthafar eða umboðsmenn þeirra sem hafa yfir að ráða helmingi hlutafjár í félaginu að minnsta kosti.  Verði fundur ólögmætur vegna annmarka að þessu leyti skal boðað til nýs fundar innan mánaðar með sjö daga fyrirvara og verður sá fundur lögmætur til að ræða þau mál sem ræða átti á fyrri fundinum ef hann sækja  hluthafar eða umboðsmenn þeirra sem ráða yfir 10% hlutafjár í félaginu hið minnsta.'
+    },
+    {
+      number: 12,
+      text: 'Eitt atkvæði fylgir hverri 1 krónu í hlutafé.  Hluthafar geta með skriflegu umboði veitt umboðsmönnum heimild til að sækja hluthafafund og fara með atkvæðisrétt sinn. Á hluthafafundi ræður afl atkvæða nema öðruvísi sé fyrir mælt í landslögum eða samþykktum þessum.  Samþykki allra hluthafa þarf til þess: a.	að skylda hluthafa til að leggja fram fé í félagsþarfir fram yfir skuldbindingar sínar, b.	að takmarka heimild manna til meðferðar á hlutum sínum eða c.	að breyta ákvæðum samþykktanna um hlutdeild manna í félaginu eða jafnrétti þeirra á milli. Tillögur um breytingar á samþykktum félagsins eða um sameiningu þess við önnur félög eða fyrirtæki má ekki taka til meðferðar á fundum þess nema þess hafi verið getið í fundarboði.'
+    },
+    {
+      number: 13,
+      text: '	Á aðalfundi skulu tekin fyrir þessi mál: 1.	Stjórn félagsins skal skýra frá hag félagsins og rekstri þess á liðnu starfsári. 2.	Efnahagsreikningur og rekstrarreikningur félagsins fyrir liðið starfsár skulu lagðir fram ásamt athugasemdum endurskoðenda eða skoðunarmanna félagsins til samþykktar. 3.	Stjórn félagsins skal kjörin og endurskoðendur eða skoðunarmenn. 4.	Tekin skal ákvörðun um hvernig fara skal með hagnað eða tap og um arð og framlög í varasjóð. 5.	Ákvörðun skal tekin um greiðslur til stjórnar- og skoðunarmanna fyrir störf þeirra á starfsárinu. 6.	Umræður og atkvæðagreiðslur um önnur málefni sem löglega eru upp borin. Verði hluthafi einn skal hann taka ákvarðanir skv. 59. gr. ehfl.'
+    },
+    {
+      number: 14,
+      text: '	Fundargerðarbók skal haldin og í hana skráð það sem gerist á hluthafafundum. Sé hluthafi einn skráir hann ákvarðanir í gerðabók.'
+    },
+    {
+      number: 15,
+      text: 'Stjórn félagsins skal skipuð einum til þremur mönnum, kjörnum á aðalfundi til eins árs í senn, þó eigi færri en þremur séu hluthafar fimm eða fleiri.  Heimilt er að kjósa allt að þrjá varamenn. Skipi stjórnina einn maður skal jafnframt kjörinn a.m.k. einn varamaður til sama tíma.  Stjórn félagsins stýrir öllum málefnum félagsins milli hluthafafunda og gætir hagsmuna þess gagnvart þriðja manni.  Í fjölskipaðri stjórn skuldbinda undirskriftir meirihluta stjórnar félagið en í eins manns stjórn undirskrift stjórnarmannsins. Stjórnarfundir eru lögmætir ef meirihluti stjórnarmanna sækir fund.  Afl atkvæða ræður afgreiðslu mála. Verði atkvæði jöfn ræður atkvæði formanns úrslitum. Halda skal fundargerð um stjórnarfund. Ef stjórn félagsins er aðeins skipuð einum manni og framkvæmdastjóri er í félaginu skal stjórnarmaðurinn að jafnaði taka mikilvægar ákvarðanir í málefnum félagsins að höfðu samráði við framkvæmdastjórann, sé hann eigi sami maður, og skrá þær í gerðabók félagsins.'
+    },
+    {
+      number: 16,
+      text: 'Sé stjórnin fjölskipuð skiptir hún sjálf með sér verkum.  Formaður boðar til stjórnarfunda.  Hver stjórnarmaður getur krafist stjórnarfundar.  Sama rétt á framkvæmdastjóri. Stjórnin skal setja sér starfsreglur þar sem nánar skal kveðið á um framkvæmd starfa hennar.'
+    },
+    {
+      number: 17,
+      text: 'Stjórn félagsins ræður framkvæmdastjóra og ákveður starfskjör hans.  Hún veitir og prókúruumboð fyrir félagið.  Skipi einn maður stjórnina getur hann jafnframt verið framkvæmdastjóri, sbr. 41. gr. ehfl. Framkvæmdastjóri hefur með höndum stjórn á daglegum rekstri félagsins og kemur fram fyrir þess hönd í öllum málum sem varða venjulegan rekstur.  Hann sér um reikningshald og ráðningu starfsliðs.  Framkvæmdastjóra ber að veita stjórnarmönnum og endurskoðendum eða skoðunarmönnum allar upplýsingar um rekstur félagsins sem þeir kunna að óska og veita ber samkvæmt lögum.'
+    },
+    {
+      number: 18,
+      text: 'Á aðalfundi félagsins skal kjósa einn eða fleiri löggilta endurskoðendur eða skoðunarmenn.  Skulu þeir rannsaka reikninga félagsins fyrir hvert starfsár og leggja niðurstöður sínar fyrir aðalfund.  Endurskoðendur eða skoðunarmenn má ekki kjósa úr hópi stjórnarmanna eða starfsmanna félagsins.'
+    },
+    {
+      number: 19,
+      text: 'Starfsár og reikningsár er almanaksárið. Stjórnin skal hafa lokið gerð ársreikninga og lagt fyrir endurskoðendur eða skoðunarmenn eigi síðar en einum mánuði fyrir aðalfund.'
+    },
+    {
+      number: 20,
+      text: 'Engin sérréttindi fylgja hlutum í félaginu. Hluthafar þurfa ekki að sæta innlausn hluta sinna nema lög standi til annars.'
+    },
+    {
+      number: 21,
+      text: 'Samþykktum þessum má breyta á lögmætum aðalfundi eða aukafundi með 2/3 hlutum greiddra atkvæða, svo og með samþykki hluthafa sem ráða yfir a.m.k. 2/3 hlutum af því hlutafé í félaginu sem farið er með atkvæði fyrir á fundinum, enda sé annað atkvæðamagn ekki áskilið í samþykktum eða landslögum, sbr. 68. gr. ehfl. Sé hluthafi einn getur hann breytt samþykktun þessum með skráningu ákvörðunar í gerðabók, sbr. 55. og 59. gr. ehfl.'
+    },
+    {
+      number: 22,
+      text: 'Með tillögur um slit og skipti á félaginu skal fara sem um breytingar á samþykktum þessum.  Þarf atkvæði hluthafa sem ráða minnst 2/3 hlutum af heildarhlutafé félagsins til að ákvörðun um slit sé gild.  Hluthafafundur, sem tekið hefur löglega ákvörðun um slit eða skipti félagsins, skal einnig ákveða ráðstöfun eigna og greiðslu skulda.'
+    },
+    {
+      number: 23,
+      text: 'Þar sem ákvæði samþykkta þessara segja ekki til um hvernig með skuli farið skal hlíta ákvæðum laga um einkahlutafélög, svo og öðrum lagaákvæðum er við geta átt.'
+    },
+  ],
+  endnote: {
+    endnote: 'Þannig samþykkt hinn '
   },
-  {
-    number: 2,
-    text: 'Tilgangur félagsins er '
-  },
-  {
-    number: 3,
-    text: 'Hlutafé félagsins er kr. 500.000 - krónur fimm hundruð þúsund. Hver hlutur er að fjárhæð kr. 1 - króna  að nafnverði.'
-  },
-  {
-    number: 4,
-    text: 'Heimilt er að hækka hlutafé félagsins með ályktun hluthafafundar og þarf til sama magn atkvæða og til breytinga á samþykktum þessum. Hluthafar skulu hafa forgangsrétt að nýjum hlutum í hlutfalli við skráða hlutafjáreign sína. Hlutahafafundur einn getur ákveðið lækkun hlutafjár. Sé hluthafi einn getur hann að lagaskilyrðum fullnægðum skráð hækkun eða lækkun hlutafjár í gerðabók.'
-  }],
 }
 
 module.exports = SAMTHYKKT;
